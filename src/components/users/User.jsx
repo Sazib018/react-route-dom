c0a05773e958dde9a8d8e9dbcd648159831d4b5c
Sazib <mahiburislamsazib@gmail.com>
2024-10-23 00:01:22 +0600

    done

--- src/components/users/User.jsx
+++ src/components/users/User.jsx
@@ -6,10 +6,10 @@ const User = ({ user }) => {
 
     return (
         <div className='border-2 border-orange-700 p-6 rounded-lg text-start'>
-            <h1 className='text-2xl'>Name: {name}</h1>
-            <p>Email: {email}</p>
-            <p>Mobile No: {phone}</p>
-            <small className='block'>Website Link: {website}</small>
+            <h1 className='text-2xl'><strong>Name:</strong> {name}</h1>
+            <p><strong>Email:</strong> {email}</p>
+            <p><strong>Mobile No:</strong> {phone}</p>
+            <small className='block'><strong>Website Link:</strong> {website}</small>
             <Link to={`/users/${id}`}>
                 <button className='bg-green-600 py-3 px-6 rounded-md mt-4 font-semibold text-white'>
                     See Details
